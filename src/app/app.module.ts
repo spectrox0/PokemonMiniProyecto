92fc22635558e05bfbfd7ca438694632b8e97d67
YAlejandro Velazco <tim@timrich.org>
2018-10-12 11:54:41 -0400

    asa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { DetallesComponent } from './componentes/detalles/detalles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetallesComponent
   ],
   imports: [
     BrowserModule,
