940b850199098a62ec3bfc87f23373feb3c46e70
YAlejandro Velazco <tim@timrich.org>
2018-10-12 11:54:55 -0400

    cambio32

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { DetallesComponent } from './componentes/detalles/detalles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '' , redirectTo : '/home' , pathMatch: 'full' } ,
+    {path: 'home', component: HomeComponent},
+    {path: 'detalles', component: DetallesComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
